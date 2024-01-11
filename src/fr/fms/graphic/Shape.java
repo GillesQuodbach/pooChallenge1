ccd6d78983a029c777a50b4010bf7650da96fe86
Gilles <quodbach.gilles@gmail.com>
2024-01-11 09:20:26 +0100

    fichiers de base

--- src/fr/fms/graphic/Shape.java
+++ src/fr/fms/graphic/Shape.java
@@ -1,5 +1,18 @@
 package fr.fms.graphic;
 
 public class Shape {
+private Point center;
+
+public Shape(int x, int y) {
+	this.center = new Point(x,y);
+}
+
+public Shape(Point center) {
+	this.center = new Point(center.getX(), center.getY());
+}
+
+public String toString() {
+	return " Shape [center = " + center + "]";
+}
 
 }
--- src/fr/fms/graphic/TestShapes.java
+++ src/fr/fms/graphic/TestShapes.java
@@ -3,7 +3,13 @@ package fr.fms.graphic;
 public class TestShapes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Circle c1 = new Circle(2.0,3,7);
+		System.out.println(c1);
+		
+		Square s1 = new Square(3,3,7);
+		System.out.println(s1);
+		
+		
 
 	}
 
